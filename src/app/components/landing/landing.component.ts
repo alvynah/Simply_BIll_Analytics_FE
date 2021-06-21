8c38f52650396e15d4deeebaf436cfcbbab8fed5
alvynah <alvynahw@gmail.com>
2021-06-21 17:04:45 +0300

    change id fields

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-
-  constructor() { }
+  user: any = []
+  message = " "
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.http.get('http://localhost:8000/api/user/current-user/',
+      {withCredentials: true})
+      .subscribe(response => {
+        console.log(response);
+        this.user = response;
+        console.log(this.user.is_valid);
+      },
+        error => {
+          console.log('error', error)
+          this.message = "no user found";
+          //redirect
+        }
+      );
   }
 
 }
