7113aafdddbad2b74bf36663643d43903b5598d6
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-06-22 21:16:42 +0300

    Admin Login and Activation

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -12,57 +12,6 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class RegistrationComponent implements OnInit {
 
-<<<<<<< HEAD
-  form:any = FormGroup ;
-
-  errors:any
-  submitted:boolean=false;
-  loading:boolean = false;
-
-  // form =new FormGroup({
-  //   first_name:new FormControl('', [Validators.required]),
-  //   last_name:new FormControl('', [Validators.required]),
-  //   username:new FormControl('', [Validators.required]),
-  //   email:new FormControl('', [Validators.required]),
-  //   password:new FormControl('', [Validators.required]),
-  // });
-  // response: any;
-  
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private http: HttpClient,
-    private router: Router,
-    
-    ){ 
-  }
-
-  ngOnInit(): void {
-    // this.form = this.formBuilder.group({
-    //   first_name: '',
-    //   last_name : '',
-    //   email:'',
-    //   phone_number:'',
-    //   password:'',
-    //   username:'',
-    // }); 
-    this.form =new FormGroup({
-      first_name:new FormControl('', [Validators.required]),
-      last_name:new FormControl('', [Validators.required]),
-      username:new FormControl('', [Validators.required]),
-      phone_number:new FormControl('', [Validators.required]),
-      email:new FormControl('', [Validators.required]),
-      password:new FormControl('', [Validators.required]),
-      });
-    }
- 
-  submit(): void{
-    this.http.post('http://localhost:8000/api/user/register/',this.form.getRawValue()).subscribe(response =>{console.log(response)
-      alert("You have been Successfully Registered")
-      this.router.navigateByUrl('/login');
-    })
-  }
-=======
         form! : FormGroup ;
 
         errors:any
@@ -133,7 +82,6 @@ export class RegistrationComponent implements OnInit {
               
         }
 
->>>>>>> 6f7f786542decaf7e26f21cbb24ab9a5e9e83b4b
 
   // submit(): void{
   //   this.submitted=true
